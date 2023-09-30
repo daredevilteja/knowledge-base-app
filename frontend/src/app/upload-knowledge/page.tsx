448c48e470fb0e72b32b9140d8c323ffb6bfa3ae
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2023-10-01 00:15:01 +0530

    Fixed styles

--- frontend/src/app/upload-knowledge/page.tsx
+++ frontend/src/app/upload-knowledge/page.tsx
@@ -8,12 +8,17 @@ export default function UploadKnowledge() {
   const [metaphorQuery, setMetaphorQuery] = useState("");
   const [numResults, setNumResults] = useState(20);
   const [startPublishedDate, setStartPublishedDate] = useState("2021-01-01");
+  const [includeDomains, setIncludeDomains] = useState("");
 
-  const fetchData = async () => {
+  const fetchData = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     setLoading(true);
     try {
       const response = await fetch(
-        `http://localhost:8000/api/internet-search?query=${metaphorQuery}&num_results=${numResults}&start_published_date=${startPublishedDate}`,
+        `http://localhost:8000/api/internet-search?query=${metaphorQuery}&num_results=${numResults}&start_published_date=${startPublishedDate}` +
+          `${
+            includeDomains !== "" ? `&include_domains=${includeDomains}` : ""
+          }`,
         {
           method: "GET",
           headers: {
@@ -33,6 +38,10 @@ export default function UploadKnowledge() {
       alert("Error occurred while sending the request");
     } finally {
       setLoading(false);
+      setMetaphorQuery("");
+      setIncludeDomains("");
+      setNumResults(20);
+      setStartPublishedDate("2021-01-01");
     }
   };
   return (
@@ -40,19 +49,26 @@ export default function UploadKnowledge() {
       {loading ? (
         <Loader />
       ) : (
-        <section className="flex flex-col flex-8 px-4 py-6 gap-8">
+        <form
+          onSubmit={fetchData}
+          className="flex flex-col flex-8 px-4 py-2 gap-8"
+        >
+          <p className="text-2xl font-semibold text-center">Load Data</p>
           <label className="flex gap-4 items-center">
             Enter a query to fetch data
             <input
               type="text"
+              className="w-1/2"
               placeholder="What are the recent blogs on react state management?"
               onChange={(e) => setMetaphorQuery(e.target.value)}
+              required
             />
           </label>
           <label className="flex gap-4 items-center">
             Number of results you want to fetch
             <input
               type="number"
+              className="w-1/12"
               placeholder="20"
               onChange={(e) => setNumResults(Number(e.target.value))}
               max={20}
@@ -62,21 +78,25 @@ export default function UploadKnowledge() {
             Date from which you want to search
             <input
               type="date"
+              className="w-2/12"
               defaultValue={"2021-01-01"}
               onChange={(e) => setStartPublishedDate(e.target.value)}
             />
           </label>
           <label className="flex gap-4 items-center">
             Domains from which you want to extract information (use comma
-            seperated values) <b>Optional</b>
+            seperated values)
             <input
               type="text"
+              className="w-1/4"
               placeholder={"https://dev.to, https://www.medium.com"}
-              onChange={(e) => setStartPublishedDate(e.target.value)}
+              onChange={(e) => setIncludeDomains(e.target.value)}
             />
           </label>
-          <button onClick={fetchData}>Submit</button>
-        </section>
+          <div className="flex justify-center">
+            <button type="submit">Submit</button>
+          </div>
+        </form>
       )}
     </>
   );
