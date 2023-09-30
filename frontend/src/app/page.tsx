3b260981b81c5e8e27b5badff29810309865e1f1
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2023-10-01 00:41:52 +0530

    Fixed errors

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,14 +9,15 @@ export default function Home() {
   const [startPublishedDate, setStartPublishedDate] = useState("2021-01-01");
   const [includeDomains, setIncludeDomains] = useState("");
 
-  const fetchData = async () => {
-    const domainsArray = includeDomains.split(",");
-    console.log(domainsArray.length);
-    domainsArray.forEach((ele) => console.log(ele));
+  const fetchData = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     setLoading(true);
     try {
       const response = await fetch(
-        `http://localhost:8000/api/internet-search?query=${metaphorQuery}&num_results=${numResults}&start_published_date=${startPublishedDate}&include_domains=${domainsArray}`,
+        `http://localhost:8000/api/internet-search?query=${metaphorQuery}&num_results=${numResults}&start_published_date=${startPublishedDate}` +
+          `${
+            includeDomains !== "" ? `&include_domains=${includeDomains}` : ""
+          }`,
         {
           method: "GET",
           headers: {
@@ -47,11 +48,16 @@ export default function Home() {
       {loading ? (
         <Loader />
       ) : (
-        <section className="flex flex-col flex-8 px-4 py-6 gap-8">
+        <form
+          onSubmit={fetchData}
+          className="flex flex-col flex-8 px-4 py-2 gap-8"
+        >
+          <p className="text-2xl font-semibold text-center">Load Data</p>
           <label className="flex gap-4 items-center">
             Enter a query to fetch data
             <input
               type="text"
+              className="w-1/2"
               placeholder="What are the recent blogs on react state management?"
               onChange={(e) => setMetaphorQuery(e.target.value)}
               required
@@ -61,6 +67,7 @@ export default function Home() {
             Number of results you want to fetch
             <input
               type="number"
+              className="w-1/12"
               placeholder="20"
               onChange={(e) => setNumResults(Number(e.target.value))}
               max={20}
@@ -70,21 +77,25 @@ export default function Home() {
             Date from which you want to search
             <input
               type="date"
+              className="w-2/12"
               defaultValue={"2021-01-01"}
               onChange={(e) => setStartPublishedDate(e.target.value)}
             />
           </label>
           <label className="flex gap-4 items-center">
             Domains from which you want to extract information (use comma
-            seperated values) <b>Optional</b>
+            seperated values)
             <input
               type="text"
+              className="w-1/4"
               placeholder={"https://dev.to, https://www.medium.com"}
               onChange={(e) => setIncludeDomains(e.target.value)}
             />
           </label>
-          <button onClick={fetchData}>Submit</button>
-        </section>
+          <div className="flex justify-center">
+            <button type="submit">Submit</button>
+          </div>
+        </form>
       )}
     </>
   );
