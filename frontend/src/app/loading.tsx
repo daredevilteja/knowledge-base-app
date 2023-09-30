067ac0136b704f884eeb3c47cf12a2763e9d3474
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2023-09-30 21:22:49 +0530

    Added loading state on route change

---
+++ frontend/src/app/loading.tsx
@@ -0,0 +1,11 @@
+const Loader = () => {
+  return (
+    <div className="loader-container">
+      <svg className="loader" viewBox="0 0 100 100">
+        <circle className="loader-circle" cx="50" cy="50" r="45" />
+      </svg>
+    </div>
+  );
+};
+
+export default Loader;
